33d87218ecb563cf35bb6dc359e17e1f6101b9a0
Dries Desmet <dries@urga.be>
2015-02-13 18:06:53 +0100

    Update to Django 1.5

--- fbgallery/urls.py
+++ fbgallery/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
 from django.conf import settings
-from django.contrib import admin
 
 fb_id = getattr(settings, 'FB_PAGE_ID', None)
 
 urlpatterns = patterns('fbgallery.views',
-    (r'^$', 'display_albums', {'fb_id':fb_id,}, 'fb-albums'),
-    (r'^(?P<album_id>[-\w]+)/', 'display_album', {'fb_id':fb_id,}, 'fb-album'),
+    (r'^$', 'display_albums', {'fb_id': fb_id}, 'fb-albums'),
+    (r'^(?P<album_id>[-\w]+)/', 'display_album', {'fb_id': fb_id}, 'fb-album'),
 )
