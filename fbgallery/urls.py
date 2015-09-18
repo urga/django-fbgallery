65f0129792f4de4ec1019f080c6bfe0bbbb2e81b
Dries Desmet <dries@urga.be>
2015-09-18 20:37:06 +0200

    Fix issue where url keeps on capturing

--- fbgallery/urls.py
+++ fbgallery/urls.py
@@ -3,7 +3,8 @@ from django.conf import settings
 
 fb_id = getattr(settings, 'FB_PAGE_ID', None)
 
-urlpatterns = patterns('fbgallery.views',
+urlpatterns = patterns(
+    'fbgallery.views',
     (r'^$', 'display_albums', {'fb_id': fb_id}, 'fb-albums'),
-    (r'^(?P<album_id>[-\w]+)/', 'display_album', {'fb_id': fb_id}, 'fb-album'),
+    (r'^(?P<album_id>[-\w]+)/$', 'display_album', {'fb_id': fb_id}, 'fb-album'),
 )
